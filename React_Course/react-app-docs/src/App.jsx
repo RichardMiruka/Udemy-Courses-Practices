ba21163b36c7976ef7ce4f333d18db2e23bf3b3d
RichardMiruka <richardonsare15@gmail.com>
2024-06-12 16:15:51 +0300

    added some comments to show nesting in react

--- React_Course/react-app-docs/src/App.jsx
+++ React_Course/react-app-docs/src/App.jsx
@@ -9,7 +9,7 @@ function MyButton() {
 
 // Now that we have declared MyButton,
 // we can nest it into another component:
-export default function MyApp() {
+export default function MyApp() { // This is the main component in the file
   return (
     <div>
       <h1>Welcome to my App</h1>
@@ -18,3 +18,7 @@ export default function MyApp() {
     </div>
   );
 }
+
+// Notice that <MyButton /> starts with a capital letter.
+// That’s how you know it’s a React component.
+// React component names must always start with a capital letter
