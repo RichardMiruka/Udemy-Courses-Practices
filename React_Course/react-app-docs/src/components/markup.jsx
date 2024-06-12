6c947bc55a864febdfe1d398751ed03aef73ecf5
RichardMiruka <richardonsare15@gmail.com>
2024-06-12 16:31:35 +0300

    writing markup with jsx in react - the rules

---
+++ React_Course/react-app-docs/src/components/markup.jsx
@@ -0,0 +1,14 @@
+// You have to close tags like < br />.
+// Your component also can’t return multiple JSX tags.
+// You have to wrap them into a shared parent,
+// like a < div >...</div > or an empty <>...</> wrapper:
+function AboutPage () {
+    return (
+        <>
+            <h1>About</h1>
+            <p> This is an about page <br /> What do you want to know about us?</p>
+            </>
+    );
+}
+
+export default AboutPage;
